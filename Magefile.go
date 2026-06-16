bdef8a72fd0e90ec4959c7f198bcad0f86a68998
Arun Poudel <arun.poudel@banenor.no>
2026-06-16 14:56:45 +0000

    feat: enable CGO

--- Magefile.go
+++ Magefile.go
@@ -11,6 +11,14 @@ import (
 // Hello prints a message (shows that you can define custom Mage targets).
 func Hello() {
 	fmt.Println("hello plugin developer!")
+}	
+
+func init() {
+    build.SetBeforeBuildCallback(func(c build.Config) (build.Config, error) {
+        fmt.Printf("Running before build callback with config: %+v\n", c)
+        c.EnableCGo = true
+        return c, nil
+    })
 }
 
 // Default configures the default target.
